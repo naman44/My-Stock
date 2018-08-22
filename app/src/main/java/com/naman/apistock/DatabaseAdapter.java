ff62e78b3810c12aee376aa907c376f96e51f2aa
Naman-PC <naman.goel@outlook.in>
2018-08-22 11:48:17 +0530

    basic structure

--- app/src/main/java/com/naman/apistock/DatabaseAdapter.java
+++ app/src/main/java/com/naman/apistock/DatabaseAdapter.java
@@ -1,16 +1,30 @@
 package com.naman.apistock;
 
+import android.content.Context;
+
 import com.naman.apistock.Model.Inventory;
 import com.naman.apistock.Model.Product;
 import com.naman.apistock.Model.RawMaterial;
 
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Product.class, RawMaterial.class, Inventory.class}, version = 1)
 public abstract class DatabaseAdapter extends RoomDatabase {
 
+    private static DatabaseAdapter dbAdapter;
     public abstract ProductDao productDao();
     public abstract RawDao rawDao();
     public abstract InventoryDao inventoryDao();
+
+    public static DatabaseAdapter getInstance(Context context){
+        if(dbAdapter == null){
+            synchronized (context){
+                dbAdapter = Room.databaseBuilder(context,
+                        DatabaseAdapter.class, "sample-db").fallbackToDestructiveMigration().build();
+            }
+        }
+        return dbAdapter;
+    }
 }
